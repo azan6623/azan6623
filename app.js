f240b155e55a6a7b6a6fd21e658f89045f04f590
azan6623 <143283273+azan6623@users.noreply.github.com>
2023-10-20 02:08:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,78 +1,33 @@
-let startBtn = document.getElementById('start');
-let stopBtn = document.getElementById('stop');
-let resetBtn = document.getElementById('reset');
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
-
-startBtn.addEventListener('click', function () {
-    timer = true;
-    stopWatch();
-});
-
-stopBtn.addEventListener('click', function () {
-    timer = false;
-});
-
-resetBtn.addEventListener('click', function () {
-    timer = false;
-    hour = 0;
-    minute = 0;
-    second = 0;
-    count = 0;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
-    document.getElementById('sec').innerHTML = "00";
-    document.getElementById('count').innerHTML = "00";
-});
-
-function stopWatch() {
-    if (timer) {
-        count++;
-
-        if (count == 100) {
-            second++;
-            count = 0;
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
         }
 
-        if (second == 60) {
-            minute++;
-            second = 0;
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
         }
 
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-            second = 0;
-        }
-
-        let hrString = hour;
-        let minString = minute;
-        let secString = second;
-        let countString = count;
-
-        if (hour < 10) {
-            hrString = "0" + hrString;
-        }
-
-        if (minute < 10) {
-            minString = "0" + minString;
-        }
-
-        if (second < 10) {
-            secString = "0" + secString;
-        }
-
-        if (count < 10) {
-            countString = "0" + countString;
-        }
-
-        document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
-        document.getElementById('sec').innerHTML = secString;
-        document.getElementById('count').innerHTML = countString;
-        setTimeout(stopWatch, 10);
+        document.querySelector("#newtask input").value = "";
     }
 }
